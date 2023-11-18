6bd5ceaa358ffd6f2ed9041b4470946b0c830470
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2023-11-18 02:19:28 +0100

    mved GLSL code into JavaScript

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,7 +1,50 @@
+const vertexCode = `\
+#version 300 es
+
+in vec4 vertexPosition;
+
+void main() {
+  gl_Position = vertexPosition;
+}
+`;
+
+const fragmentCode = `\
+#version 300 es
+precision highp float;
+
+uniform vec2 canvasSize;
+uniform vec3 viewPort;
+out vec4 fragColor;
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main() {
+  vec2 z = vec2(0.0, 0.0);
+  float x = gl_FragCoord.x / canvasSize.y * viewPort.z + viewPort.x;
+  float y = gl_FragCoord.y / canvasSize.y * viewPort.z + viewPort.y;
+
+  for (int i = 0; i < 1000; ++ i) {
+    float zx = z.x*z.x - z.y*z.y + x;
+    z.y = 2.0 * z.x*z.y + y;
+    z.x = zx;
+    if (abs(zx) > 4.0) {
+      float v = float(i) / 1000.0;
+
+      fragColor.xyz = hsv2rgb(vec3(mod(v + 0.6, 1.0), 0.75, 1.0));
+      fragColor.w = 1.0;
+      return;
+    }
+  }
+  fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
+`;
 
 const canvas = document.getElementById("canvas");
-const vertexCode = document.getElementById("vertex").textContent;
-const fragmentCode = document.getElementById("fragment").textContent;
+
 let redraw;
 
 function resizeCanvas() {
